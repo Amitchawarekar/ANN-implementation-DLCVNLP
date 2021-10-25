6b7bc7d491a469622c83e8790befba5d7d65e4f3
Amit <amit.chawarekar@gmail.com>
2021-10-25 14:50:28 +0530

    updated training.py for callback implmentation

--- src/training.py
+++ src/training.py
@@ -1,8 +1,10 @@
 import os
-
+import pandas as pd
 from utils.common import read_config
 from utils.data_mgmt import get_data
 from utils.model import create_model, save_model
+from utils.save_plot import save_plot
+from utils.callbacks import get_callbacks
 import argparse
 
 def training(config_path):
@@ -21,16 +23,29 @@ def training(config_path):
     EPOCHS = config["params"]["epochs"]
     VALIDATION_SET = (X_valid, y_valid)
 
+    CALLBACK_LIST = get_calbacks(config, X_train)
+
     history = model.fit(X_train_full, y_train_full, epochs=EPOCHS,
-                    validation_data=VALIDATION_SET)
+                    validation_data=VALIDATION_SET, callbacks = CALLBACK_LIST)
 
     artifacts_dir = config["artifacts"]["artifacts_dir"]
     model_dir = config["artifacts"]["model_dir"]
 
     model_dir_path = os.path.join(artifacts_dir,model_dir)
-    os.mkdir(model_dir_path,exist_ok =True)
+    os.makedirs(model_dir_path,exist_ok =True)
+
     model_name = config["artifacts"]["model_name"]
-    save_model(model, model_name, model_dir)
+
+    save_model(model, model_name, model_dir_path)
+   
+    plots_dir = config["artifacts"]["plots_dir"]
+    plots_name = config["artifacts"]["plot_name"]
+
+    plot_dir_path = os.path.join(artifacts_dir,plots_dir)
+    os.makedirs(plot_dir_path,exist_ok =True)
+    
+
+    save_plot(pd.DataFrame(history.history), plots_name, plot_dir_path)
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
